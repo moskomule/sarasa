f51f2f520a3f2d33627f4130cf09e3ef4509babe
Ryuichiro Hataya <>
2026-01-28 13:25:36 +0900

    Refactor logger setup into a dedicated function and enhance rank handling in distributed training

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-import os
-import sys
-
-from loguru import logger
-
 from sarasa import Config, Trainer
+from sarasa.utils import setup_logger
 
 if __name__ == "__main__":
     config = Config.from_cli()
-    logger.remove()
-    rank = int(os.environ.get("RANK", 0))
 
-    if config.debug:
-        logger_format = f"<blue>RANK={rank}</blue> | " + (
-            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
-            "<level>{level: <8}</level> | "
-            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
-            "<level>{message}</level>"
-        )
-        logger.add(
-            sys.stderr,
-            format=logger_format,
-            backtrace=True,
-            diagnose=True,
-            level="DEBUG",
-        )
-    else:
-        # log to stderr only for rank 0
-        logger.add(sys.stderr, backtrace=True, diagnose=True, level="INFO" if rank == 0 else "ERROR")
+    setup_logger(config)
     trainer = Trainer(config)
     trainer.train()
--- sarasa/utils.py
+++ sarasa/utils.py
@@ -1,6 +1,7 @@
 import contextlib
 import gc
 import os
+import sys
 import time
 from datetime import timedelta
 from functools import cache
@@ -9,6 +10,27 @@ import torch
 from loguru import logger
 from torch import distributed as dist
 from torch import nn
+
+
+def setup_logger(config) -> None:
+    logger.remove()
+    if config.debug:
+        logger_format = f"<blue>RANK={rank()}</blue> | " + (
+            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+            "<level>{level: <8}</level> | "
+            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+            "<level>{message}</level>"
+        )
+        logger.add(
+            sys.stderr,
+            format=logger_format,
+            backtrace=True,
+            diagnose=True,
+            level="DEBUG",
+        )
+    else:
+        # log to stderr only for rank 0
+        logger.add(sys.stderr, backtrace=True, diagnose=True, level="INFO" if rank() == 0 else "ERROR")
 
 
 @contextlib.contextmanager
@@ -53,6 +75,9 @@ class GarbageCollector:
 
 @cache
 def world_size() -> int:
+    if "WORLD_SIZE" in os.environ:
+        return int(os.environ["WORLD_SIZE"])
+
     if not dist.is_initialized():
         return 1
     return dist.get_world_size()
@@ -60,6 +85,9 @@ def world_size() -> int:
 
 @cache
 def rank() -> int:
+    if "RANK" in os.environ:
+        return int(os.environ["RANK"])
+
     if not dist.is_initialized():
         return 0
     return dist.get_rank()
