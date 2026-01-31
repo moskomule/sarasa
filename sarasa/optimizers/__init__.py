7d240cf330d8707acf9c4977046ea8caa0d38ce1
Ryuichiro Hataya <>
2026-01-31 12:12:38 +0900

    fix

--- sarasa/optimizers/__init__.py
+++ sarasa/optimizers/__init__.py
@@ -34,47 +34,6 @@ class AdamW:
 
 
 @dataclasses.dataclass
-class AdamH:
-    """
-    AdamH optimizer
-    """
-
-    lr: float = 1e-2
-    betas: tuple[float, float] = (0.9, 0.95)
-    rescale_to_unit_ball: bool = False
-
-    adam_lr: float | None = None
-    adam_betas: tuple[float, float] = (0.9, 0.95)
-    adam_weight_decay: float = 0
-
-    def __post_init__(self):
-        self.adam_lr = self.adam_lr or self.lr
-
-    def create(
-        self,
-        model: BaseModel,
-    ) -> torch.optim.Optimizer:
-        from .hyperball_optimizer import AdamH as Optimizer
-
-        param_groups = model.param_groups()
-        adamh = Optimizer(
-            param_groups["matrix"],
-            lr=self.lr,
-            betas=self.betas,
-            rescale_to_unit_ball=self.rescale_to_unit_ball,
-        )
-        adam = torch.optim.AdamW(
-            sum([param_groups[k] for k in param_groups if k != "matrix"], []),
-            lr=self.adam_lr,
-            betas=self.adam_betas,
-            weight_decay=self.adam_weight_decay,
-            fused=True,
-        )
-
-        return GroupedOptimizer(adamh, adam)
-
-
-@dataclasses.dataclass
 class Muon:
     """
     Muon optimizer
@@ -104,52 +63,6 @@ class Muon:
             lr=self.lr,
             weight_decay=self.weight_decay,
             momentum=self.momentum,
-            adjust_lr_fn=self.adjust_lr_fn,
-        )
-
-        adam = torch.optim.AdamW(
-            sum([param_groups[k] for k in param_groups if k != "matrix"], []),
-            lr=self.adam_lr,
-            betas=self.adam_betas,
-            weight_decay=self.adam_weight_decay,
-            fused=True,
-        )
-
-        return GroupedOptimizer(muon, adam)
-
-
-@dataclasses.dataclass
-class MuonH:
-    """
-    MuonH optimizer
-    """
-
-    lr: float = 1e-2
-    momentum: float = 0.9
-    rescale_to_unit_ball: bool = False
-
-    adam_lr: float | None = None
-    adam_betas: tuple[float, float] = (0.9, 0.95)
-    adam_weight_decay: float = 0
-
-    adjust_lr_fn: Literal["original", "match_rms_adamw"] = "match_rms_adamw"
-
-    def __post_init__(self):
-        self.adam_lr = self.adam_lr or self.lr
-
-    def create(
-        self,
-        model: BaseModel,
-    ) -> torch.optim.Optimizer:
-        from .hyperball_optimizer import MuonH as Optimizer
-
-        param_groups = model.param_groups()
-
-        muon = Optimizer(
-            param_groups["matrix"],
-            lr=self.lr,
-            momentum=self.momentum,
-            rescale_to_unit_ball=self.rescale_to_unit_ball,
             adjust_lr_fn=self.adjust_lr_fn,
         )
 
