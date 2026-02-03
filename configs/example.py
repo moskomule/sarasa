c51fd30d0d7b02928810b1efd91013816aad6011
Ryuichiro Hataya <>
2026-02-03 18:00:48 +0900

    fix default parameters

--- configs/example.py
+++ configs/example.py
@@ -1,7 +1,11 @@
 from sarasa.config import AdamW, Config, Data, LRScheduler, Model, Train
 
 config = Config.create(
-    model=Model(num_layers=12),
+    model=Model(
+        name="nanochat_gpt",
+        num_layers=12,
+        qk_norm=True,
+    ),
     train=Train(
         local_batch_size=16,
         global_batch_size=256,
