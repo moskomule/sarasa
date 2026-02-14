6ef41224ff891e47f35505a884e40caede8e629a
Ryuichiro Hataya <ryuichiro.hataya@gmail.com>
2026-02-14 06:58:32 +0000

    fix

--- sarasa/data/utils.py
+++ sarasa/data/utils.py
@@ -9,13 +9,13 @@ def prepare_varlen_metadata(
 ) -> dict[str, torch.Tensor]:
     # add metadata for varlen attention
 
-    input = input_dict["input"]  # (B, T)
+    input = input_dict["input"]  # T
 
-    (bos_positions,) = (input.flatten() == bos_token_id).nonzero(as_tuple=True)
+    (bos_positions,) = (input == bos_token_id).nonzero(as_tuple=True)
     bos_positions = bos_positions.to(torch.int32)
     cu_seq = torch.cat((
         bos_positions,
-        bos_positions.new_tensor([input.size(0) * input.size(1)]),
+        bos_positions.new_tensor([input.size(0)]),
     ))
     max_seqlen = torch.diff(cu_seq).max()
     metadata = VarlenMetaData(
